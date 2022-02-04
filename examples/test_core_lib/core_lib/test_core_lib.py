5fd18914d39ab132da61c7cb3dbfcce7fa5d7297
shubham-shay-te <shubham@shay-te.com>
2022-02-04 13:16:41 +0530

    all tests passing and logger updated

--- examples/test_core_lib/core_lib/test_core_lib.py
+++ examples/test_core_lib/core_lib/test_core_lib.py
@@ -1,15 +1,8 @@
-from memcache import Client
 from omegaconf import DictConfig
-from sqlalchemy import create_engine
 
-from core_lib.cache.cache_decorator import Cache
-from core_lib.cache.cache_handler_memcached import CacheHandlerMemcached
-from core_lib.cache.cache_registry import CacheRegistry
+from core_lib.cache.cache_handler_ram import CacheHandlerRam
 from core_lib.core_lib import CoreLib
-from core_lib.data_layers.data.data_helpers import build_url
-from core_lib.data_layers.data.db.sqlalchemy.base import Base
 from core_lib.data_layers.data.handler.sql_alchemy_data_handler_registry import SqlAlchemyDataHandlerRegistry
-from core_lib.session.jwt_token_handler import JWTTokenHandler
 from examples.test_core_lib.core_lib.data_layers.data_access.slow_large_data_data_access import SlowLargeDataDataAccess
 from examples.test_core_lib.core_lib.data_layers.data_access.test1_data_access import Test1DataAccess
 from examples.test_core_lib.core_lib.data_layers.data_access.test2_data_access import Test2DataAccess
@@ -26,10 +19,7 @@ class TestCoreLib(CoreLib):
     def __init__(self, conf: DictConfig):
         self.config = conf
 
-        # JWTTokenHandler.init(self.config.app.secret)
-
-        cache_client_memcached = CacheHandlerMemcached(Client([build_url(**self.config.memcached)]))
-        CoreLib.cache_registry.register("memcached", cache_client_memcached)
+        CoreLib.cache_registry.register("memory_cache", CacheHandlerRam())
 
         db_data_session = SqlAlchemyDataHandlerRegistry(self.config.db)
 
