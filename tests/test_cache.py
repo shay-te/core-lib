c2a5b0fd420a475930d2010ceb62b8b5fd357c03
shubham-shay-te <shubham@shay-te.com>
2022-02-07 20:33:16 +0530

    tests added for cache expire using string

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,6 +1,7 @@
 import unittest
-from datetime import timedelta
+from datetime import timedelta, datetime
 from time import sleep
+from freezegun import freeze_time
 
 from core_lib.cache.cache_decorator import Cache
 from core_lib.cache.cache_handler_ram import CacheHandlerRam
@@ -12,6 +13,7 @@ cache_client_name = "xyz"
 class TestCache(unittest.TestCase):
 
     test_value = 100
+    cache_duration = "2 seconds"
 
     @classmethod
     def setUpClass(cls):
@@ -23,6 +25,19 @@ class TestCache(unittest.TestCase):
 
     def test_cache_client_register(self):
         self.assertRaises(ValueError, self.not_exists_cache_client_name)
+
+    def test_cash(self):
+        self.clear_cache()
+        TestCache.test_value = 100
+        self.assertEqual(self.get_cache(), 100)
+        TestCache.test_value = 200
+        self.assertEqual(self.get_cache(), 100)
+        sleep(2.3)
+        self.assertEqual(self.get_cache(), 200)
+        TestCache.test_value = 100
+        self.assertEqual(self.get_cache(), 200)
+        self.clear_cache()
+        self.assertEqual(self.get_cache(), 100)
 
     def test_cash(self):
         self.clear_cache()
@@ -104,6 +119,70 @@ class TestCache(unittest.TestCase):
         sleep(1)
         self.assertEqual(self.get_cache(), 200)
 
+    def test_cache_using_string(self):
+        self.clear_cache()
+        TestCache.cache_duration = "2 seconds"
+        TestCache.test_value = 100
+        self.assertEqual(self.get_cache_expire_string(), 100)
+        TestCache.test_value = 200
+        self.assertEqual(self.get_cache_expire_string(), 100)
+        with freeze_time(datetime.today().replace(minute=datetime.now().minute + 1)):
+            self.assertEqual(self.get_cache_expire_string(), 200)
+
+        self.clear_cache()
+        TestCache.cache_duration = "60 seconds"
+        TestCache.test_value = 100
+        self.assertEqual(self.get_cache_expire_string(), 100)
+        TestCache.test_value = 200
+        self.assertEqual(self.get_cache_expire_string(), 100)
+        with freeze_time(datetime.today().replace(minute=datetime.now().minute + 1)):
+            self.assertEqual(self.get_cache_expire_string(), 200)
+
+        self.clear_cache()
+        TestCache.cache_duration = "1 minute"
+        TestCache.test_value = 100
+        self.assertEqual(self.get_cache_expire_string(), 100)
+        TestCache.test_value = 200
+        self.assertEqual(self.get_cache_expire_string(), 100)
+        with freeze_time(datetime.today().replace(minute=datetime.now().minute + 1)):
+            self.assertEqual(self.get_cache_expire_string(), 200)
+
+        self.clear_cache()
+        TestCache.cache_duration = "1 day"
+        TestCache.test_value = 100
+        self.assertEqual(self.get_cache_expire_string(), 100)
+        TestCache.test_value = 200
+        self.assertEqual(self.get_cache_expire_string(), 100)
+        with freeze_time(datetime.today() + timedelta(days=1)):
+            self.assertEqual(self.get_cache_expire_string(), 200)
+
+        self.clear_cache()
+        TestCache.cache_duration = "1 week"
+        TestCache.test_value = 100
+        self.assertEqual(self.get_cache_expire_string(), 100)
+        TestCache.test_value = 200
+        self.assertEqual(self.get_cache_expire_string(), 100)
+        with freeze_time(datetime.today() + timedelta(days=7)):
+            self.assertEqual(self.get_cache_expire_string(), 200)
+
+        self.clear_cache()
+        TestCache.cache_duration = "1 month"
+        TestCache.test_value = 100
+        self.assertEqual(self.get_cache_expire_string(), 100)
+        TestCache.test_value = 200
+        self.assertEqual(self.get_cache_expire_string(), 100)
+        with freeze_time(datetime.today().replace(month=datetime.now().month + 1)):
+            self.assertEqual(self.get_cache_expire_string(), 200)
+
+        self.clear_cache()
+        TestCache.cache_duration = "1 year"
+        TestCache.test_value = 100
+        self.assertEqual(self.get_cache_expire_string(), 100)
+        TestCache.test_value = 200
+        self.assertEqual(self.get_cache_expire_string(), 100)
+        with freeze_time(datetime.today().replace(year=datetime.now().year + 1)):
+            self.assertEqual(self.get_cache_expire_string(), 200)
+
     @Cache(key="test_cache_1", expire=timedelta(seconds=2))
     def get_cache(self):
         return TestCache.test_value
@@ -136,3 +215,6 @@ class TestCache(unittest.TestCase):
     def get_cache_only_param_optional(self, param_1=None, param_2=None, param_3=None, param_4=None):
         return TestCache.test_value
 
+    @Cache(key="test_cache_1", expire=str(cache_duration))
+    def get_cache_expire_string(self):
+        return TestCache.test_value
