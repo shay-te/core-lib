bfe28429d7f0b7c5d7f592d036532fce5b88586a
shubham-shay-te <shubham@shay-te.com>
2022-02-17 12:33:41 +0530

    cruddataaccess replaced

--- tests/test_crud_data.py
+++ tests/test_crud_data.py
@@ -5,8 +5,8 @@ from sqlalchemy import Column, Integer, VARCHAR
 from core_lib.data_layers.data.handler.sql_alchemy_data_handler_registry import SqlAlchemyDataHandlerRegistry
 from core_lib.data_layers.data_access.db.crud.crud import CRUD
 from core_lib.data_layers.data_access.db.crud.crud_data_access import CRUDDataAccess
-from core_lib.rule_validator.rule_validator import RuleValidator
 from tests.test_data.test_utils import connect_to_mem_db
+from core_lib.rule_validator.rule_validator import ValueRuleValidator, RuleValidator
 from core_lib.data_layers.data.db.sqlalchemy.base import Base
 
 
@@ -17,15 +17,20 @@ class Data(Base):
     name = Column(VARCHAR(length=255), nullable=False, default="")
 
 
-class CRUDInit(CRUD):
+class CRUDInit(CRUDDataAccess):
+    allowed_update_types = [
+        ValueRuleValidator('name', str)
+    ]
+
+    rules_validator = RuleValidator(allowed_update_types, mandatory_keys=['name'])
+
     def __init__(self):
-        CRUD.__init__(self, Data, connect_to_mem_db(), RuleValidator)
+        CRUD.__init__(self, Data, connect_to_mem_db(), CRUDInit.rules_validator)
 
 
 class TestCrud(unittest.TestCase):
 
     def test_create(self):
-        crud = CRUDInit
+        crud = CRUDInit()
         crud.create({'name': 'test_name'})
-
-
+        print(crud.get(1).name)
