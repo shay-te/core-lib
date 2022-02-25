3e62a50180428584365245a342f2442aed9f906c
shubham-shay-te <shubham@shay-te.com>
2022-02-25 14:27:51 +0530

    updated logging.py and tests

--- core_lib/helpers/logging.py
+++ core_lib/helpers/logging.py
@@ -1,34 +1,17 @@
 import logging
 from functools import wraps
 
-from core_lib.helpers.func_utils import (
-    build_value_by_func_parameters,
-    generate_key_by_func_parameters,
-)
-
 
 class Logging(object):
-    def __init__(self, message: str = '', level: int = logging.INFO, stack_depth=1, log_parameters: bool = False):
+    def __init__(self, message: str = '', level: int = logging.INFO):
         self.message = message
         self.level = level
-        self.log_parameters = log_parameters
 
     def __call__(self, func, *args, **kwargs):
         @wraps(func)
         def __wrapper(*args, **kwargs):
-            formatted_params = ""
-            message = generate_key_by_func_parameters(func, *args, **kwargs)
-            if self.log_parameters:
-                formatted_params = build_value_by_func_parameters(
-                    generate_key_by_func_parameters(func, *args, **kwargs), func, *args, **kwargs
-                )
-            if self.message:
-                message = self.message
-
             logging.basicConfig(level=self.level)
-            logging.getLogger(func.__name__).log(
-                self.level, f'{".".join(filter(None, [message, formatted_params]))}'
-            )
+            logging.getLogger(func.__qualname__).log(self.level, f'{self.message}')
             return func(*args, **kwargs)
 
         return __wrapper
--- tests/test_logging.py
+++ tests/test_logging.py
@@ -8,153 +8,75 @@ class TestLogging(unittest.TestCase):
     def test_string_logger(self):
         string = "hello"
 
-        with self.assertLogs('logging_message_params', level='INFO') as cm:
-            self.logging_message_params(string)
-        self.assertEqual(cm.output, ["INFO:logging_message_params:log_for_test.hello"])
-
-        with self.assertLogs('logging_message_params', level='INFO') as cm:
-            self.logging_message_params(None)
-        self.assertEqual(cm.output, ["INFO:logging_message_params:log_for_test.!EparamE!"])
-
-        with self.assertLogs('logging_message_params', level='INFO') as cm:
-            self.logging_message_params("")
-        self.assertEqual(cm.output, ["INFO:logging_message_params:log_for_test.!EparamE!"])
-
-        with self.assertLogs('logging_message', level='INFO') as cm:
+        with self.assertLogs('TestLogging.logging_message', level='INFO') as cm:
             self.logging_message(string)
-        self.assertEqual(cm.output, ["INFO:logging_message:log_for_test"])
+        self.assertEqual(cm.output, ["INFO:TestLogging.logging_message:log_for_test"])
 
-        with self.assertLogs('logging_params', level='INFO') as cm:
-            self.logging_params(string)
-        self.assertEqual(cm.output, ["INFO:logging_params:.hello"])
+        with self.assertLogs('TestLogging.logging_without_message', level='INFO') as cm:
+            self.logging_without_message(string)
+        self.assertEqual(cm.output, ["INFO:TestLogging.logging_without_message:"])
 
     def test_object_logger(self):
         dat = datetime.date(2022, 1, 1)
         obj = {'date': dat, 'tuple': ("fruit", "apple"), 'list': ["fruit", "apple"]}
 
-        with self.assertLogs('logging_message_params', level='INFO') as cm:
-            self.logging_message_params(obj)
-        self.assertEqual(
-            cm.output,
-            [
-                "INFO:logging_message_params:log_for_test.{'date': datetime.date(2022, 1, 1), 'tuple': ('fruit', 'apple'), 'list': ['fruit', 'apple']}"
-            ],
-        )
-
-        with self.assertLogs('logging_message_params', level='INFO') as cm:
-            self.logging_message_params({})
-        self.assertEqual(cm.output, ["INFO:logging_message_params:log_for_test.!EparamE!"])
-
-        with self.assertLogs('logging_message', level='INFO') as cm:
+        with self.assertLogs('TestLogging.logging_message', level='INFO') as cm:
             self.logging_message(obj)
-        self.assertEqual(cm.output, ["INFO:logging_message:log_for_test"])
+        self.assertEqual(cm.output, ["INFO:TestLogging.logging_message:log_for_test"])
 
-        with self.assertLogs('logging_params', level='INFO') as cm:
-            self.logging_params(obj)
+        with self.assertLogs('TestLogging.logging_without_message', level='INFO') as cm:
+            self.logging_without_message(obj)
         self.assertEqual(
             cm.output,
-            [
-                "INFO:logging_params:.{'date': datetime.date(2022, 1, 1), 'tuple': ('fruit', 'apple'), 'list': ['fruit', 'apple']}"
-            ],
+            ["INFO:TestLogging.logging_without_message:"],
         )
 
     def test_tuple_logger(self):
         tpl = ("fruit", "apple")
 
-        with self.assertLogs('logging_message_params', level='INFO') as cm:
-            self.logging_message_params(tpl)
-        self.assertEqual(cm.output, ["INFO:logging_message_params:log_for_test.('fruit', 'apple')"])
-
-        with self.assertLogs('logging_message_params', level='INFO') as cm:
-            self.logging_message_params(())
-        self.assertEqual(cm.output, ["INFO:logging_message_params:log_for_test.!EparamE!"])
-
-        with self.assertLogs('logging_message', level='INFO') as cm:
+        with self.assertLogs('TestLogging.logging_message', level='INFO') as cm:
             self.logging_message(tpl)
-        self.assertEqual(cm.output, ["INFO:logging_message:log_for_test"])
+        self.assertEqual(cm.output, ["INFO:TestLogging.logging_message:log_for_test"])
 
-        with self.assertLogs('logging_params', level='INFO') as cm:
-            self.logging_params(tpl)
-        self.assertEqual(cm.output, ["INFO:logging_params:.('fruit', 'apple')"])
+        with self.assertLogs('TestLogging.logging_without_message', level='INFO') as cm:
+            self.logging_without_message(tpl)
+        self.assertEqual(cm.output, ["INFO:TestLogging.logging_without_message:"])
 
     def test_list_logger(self):
         lst = ["fruit", "apple"]
 
-        with self.assertLogs('logging_message_params', level='INFO') as cm:
-            self.logging_message_params(lst)
-        self.assertEqual(cm.output, ["INFO:logging_message_params:log_for_test.['fruit', 'apple']"])
-
-        with self.assertLogs('logging_message_params', level='INFO') as cm:
-            self.logging_message_params([])
-        self.assertEqual(cm.output, ["INFO:logging_message_params:log_for_test.!EparamE!"])
-
-        with self.assertLogs('logging_message', level='INFO') as cm:
+        with self.assertLogs('TestLogging.logging_message', level='INFO') as cm:
             self.logging_message(lst)
-        self.assertEqual(cm.output, ["INFO:logging_message:log_for_test"])
+        self.assertEqual(cm.output, ["INFO:TestLogging.logging_message:log_for_test"])
 
-        with self.assertLogs('logging_params', level='INFO') as cm:
-            self.logging_params(lst)
-        self.assertEqual(cm.output, ["INFO:logging_params:.['fruit', 'apple']"])
+        with self.assertLogs('TestLogging.logging_without_message', level='INFO') as cm:
+            self.logging_without_message(lst)
+        self.assertEqual(cm.output, ["INFO:TestLogging.logging_without_message:"])
 
     def test_multi_param_logger(self):
         lst = ["fruit", "apple"]
         tpl = ("fruit", "apple")
 
-        with self.assertLogs('logging_message_multi_params', level='INFO') as cm:
-            self.logging_message_multi_params(param_1="hello world", param_2=lst, param_3=tpl)
-        self.assertEqual(
-            cm.output,
-            [
-                "INFO:logging_message_multi_params:log_for_test.hello world_['fruit', 'apple']_('fruit', 'apple')"
-            ],
-        )
-
-        with self.assertLogs('logging_message_multi_params', level='INFO') as cm:
-            self.logging_message_multi_params(param_1="", param_2=[], param_3=())
-        self.assertEqual(cm.output, ["INFO:logging_message_multi_params:log_for_test.!Eparam_1E!_!Eparam_2E!_!Eparam_3E!"])
-
-        with self.assertLogs('logging_message_multi_params', level='INFO') as cm:
-            self.logging_message_multi_params()
-        self.assertEqual(cm.output, ["INFO:logging_message_multi_params:log_for_test.!Eparam_1E!_!Eparam_2E!_!Eparam_3E!"])
-
-        with self.assertLogs('logging_message_multi_params', level='INFO') as cm:
-            self.logging_message_multi_params("hello world", lst, tpl)
-        self.assertEqual(
-            cm.output, ["INFO:logging_message_multi_params:log_for_test.hello world_['fruit', 'apple']_('fruit', 'apple')"]
-        )
-
-        with self.assertLogs('logging_multi_params_message', level='INFO') as cm:
+        with self.assertLogs('TestLogging.logging_multi_params_message', level='INFO') as cm:
             self.logging_multi_params_message(param_1="hello world", param_2=lst, param_3=tpl)
-        self.assertEqual(
-            cm.output, ["INFO:logging_multi_params_message:log_for_test"]
-        )
+        self.assertEqual(cm.output, ["INFO:TestLogging.logging_multi_params_message:log_for_test"])
 
-        with self.assertLogs('logging_multi_params', level='INFO') as cm:
-            self.logging_multi_params(param_1="hello world", param_2=lst, param_3=tpl)
-        self.assertEqual(
-            cm.output, ["INFO:logging_multi_params:.hello world_['fruit', 'apple']_('fruit', 'apple')"]
-        )
-
-    @Logging(message="log_for_test", log_parameters=True)
-    def logging_message_params(self, param):
-        return param
+        with self.assertLogs('TestLogging.logging_multi_params_without_message', level='INFO') as cm:
+            self.logging_multi_params_without_message(param_1="hello world", param_2=lst, param_3=tpl)
+        self.assertEqual(cm.output, ["INFO:TestLogging.logging_multi_params_without_message:"])
 
     @Logging(message="log_for_test")
     def logging_message(self, param):
         return param
 
-    @Logging(log_parameters=True)
-    def logging_params(self, param):
+    @Logging()
+    def logging_without_message(self, param):
         return param
-
-    @Logging(message="log_for_test", log_parameters=True)
-    def logging_message_multi_params(self, param_1: str = "", param_2: list = [], param_3: tuple = ()):
-        return "logged"
 
     @Logging(message="log_for_test")
     def logging_multi_params_message(self, param_1: str = "", param_2: list = [], param_3: tuple = ()):
         return "logged"
 
-    @Logging(log_parameters=True)
-    def logging_multi_params(self, param_1: str = "", param_2: list = [], param_3: tuple = ()):
+    @Logging()
+    def logging_multi_params_without_message(self, param_1: str = "", param_2: list = [], param_3: tuple = ()):
         return "logged"
