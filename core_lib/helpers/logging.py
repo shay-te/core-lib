7f44dca781d6c86fd11a05e00eb45e8258cfa4e4
shubham-shay-te <shubham@shay-te.com>
2022-02-28 12:18:04 +0530

    removed basicConfig from logging

--- core_lib/helpers/logging.py
+++ core_lib/helpers/logging.py
@@ -10,7 +10,6 @@ class Logging(object):
     def __call__(self, func, *args, **kwargs):
         @wraps(func)
         def __wrapper(*args, **kwargs):
-            logging.basicConfig(level=self.level)
             logging.getLogger(func.__qualname__).log(self.level, f'{self.message}')
             return func(*args, **kwargs)
 
