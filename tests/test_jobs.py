a20a0201ac6623469102d6b8a9815842d4d2ee55
shubham-shay-te <shubham@shay-te.com>
2022-03-01 15:11:16 +0530

    updated job tests for logging exceptions

--- tests/test_jobs.py
+++ tests/test_jobs.py
@@ -23,7 +23,7 @@ class TestJobRaiseException(Job):
         pass
 
     def run(self):
-        if TestJobRaiseException.called == 5:
+        if TestJobRaiseException.called == 3:
             raise BaseException
         TestJobRaiseException.called = TestJobRaiseException.called + 1
 
@@ -35,36 +35,46 @@ class TestJobs(unittest.TestCase):
         self.assertEqual(j.called, 1)
 
     def test_schedule_invalid(self):
-        s = JobScheduler()
-        j = TestJob()
-        self.assertRaises(AssertionError, s.schedule_once, 'asd;lasdfj', j)
-        self.assertRaises(AssertionError, s.schedule_once, None, j)
-        self.assertRaises(AssertionError, s.schedule_once, '1s', None)
-        self.assertRaises(AssertionError, s.schedule_once, '1s', 'not a job')
+        scheduler = JobScheduler()
+        job = TestJob()
+        self.assertRaises(AssertionError, scheduler.schedule_once, 'asd;lasdfj', job)
+        self.assertRaises(AssertionError, scheduler.schedule_once, None, job)
+        self.assertRaises(AssertionError, scheduler.schedule_once, '1s', None)
+        self.assertRaises(AssertionError, scheduler.schedule_once, '1s', 'not a job')
 
-        self.assertRaises(AssertionError, s.schedule, '1s', None, j)
-        self.assertRaises(AssertionError, s.schedule, '1s', 'asdasdasd', j)
-        self.assertRaises(AssertionError, s.schedule, '1s', '2s', None)
-        self.assertRaises(AssertionError, s.schedule, '1s', '2s', 'not a job')
+        self.assertRaises(AssertionError, scheduler.schedule, '1s', None, job)
+        self.assertRaises(AssertionError, scheduler.schedule, '1s', 'asdasdasd', job)
+        self.assertRaises(AssertionError, scheduler.schedule, '1s', '2s', None)
+        self.assertRaises(AssertionError, scheduler.schedule, '1s', '2s', 'not a job')
 
     def test_schedule(self):
-        s = JobScheduler()
-        j = TestJob()
-        s.schedule_once('1s', j)
+        scheduler = JobScheduler()
+        job = TestJob()
+        scheduler.schedule_once('1s', job)
         sleep(2)
-        self.assertEqual(j.called, 1)
+        self.assertEqual(job.called, 1)
 
-        s.schedule('1s', '1s', j)
+        scheduler.schedule('1s', '1s', job)
         sleep(5)
-        s.stop(j)
-        self.assertGreater(j.called, 2)
+        scheduler.stop(job)
+        self.assertGreater(job.called, 2)
 
     def test_schedule_log(self):
-        s = JobScheduler()
+        scheduler = JobScheduler()
         job_exception = TestJobRaiseException()
         with self.assertLogs() as cm:
-            s.schedule('1s', '1s', job_exception)
+            scheduler.schedule('1s', '1s', job_exception)
             sleep(10.1)
+            scheduler.stop(job_exception)
+            log = str(cm.output)
+            self.assertIn('BaseException', log)
+            self.assertIn('Error while running job', log)
+
+        with self.assertLogs() as cm:
+            scheduler.schedule('10s', '5s', job_exception)
+            sleep(25.1)
+            scheduler.stop(job_exception)
+            self.assertEqual(job_exception.called, 3)
             log = str(cm.output)
             self.assertIn('BaseException', log)
             self.assertIn('Error while running job', log)
