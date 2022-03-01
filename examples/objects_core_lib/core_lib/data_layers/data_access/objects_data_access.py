6ab51226673af1839cc31805c7af16113509a520
shubham-shay-te <shubham@shay-te.com>
2022-03-01 18:38:03 +0530

    updated objects core lib and tests

--- examples/objects_core_lib/core_lib/data_layers/data_access/objects_data_access.py
+++ examples/objects_core_lib/core_lib/data_layers/data_access/objects_data_access.py
@@ -12,14 +12,14 @@ class ObjectsDataAccess(DataAccess):
         self.logger = logging.getLogger(self.__class__.__name__)
         self.data_session_factory = data_session_factory
 
-    def get_object(self, bucket_name: str, object_name: str):
-        file_object = io.BytesIO()
+    def get_object(self, bucket_name: str, object_name: str, key: str):
         with self.data_session_factory.get() as s3:
-            s3.download_fileobj(bucket_name, object_name, file_object)
-        return file_object
+            with open(object_name, 'wb') as data:
+                s3.download_fileobj(bucket_name, key, data)
+                return data
 
-    def set_object(self, bucket_name: str, value):
-        with open(value, "rb") as f:
+    def set_object(self, bucket_name: str, value, key: str):
+        with open(value, "rb") as file:
             with self.data_session_factory.get() as session:
-                session.upload_file(f.name, bucket_name, value)
+                session.upload_fileobj(file, bucket_name, key)
 
--- tests/test_example_objectscorelib.py
+++ tests/test_example_objectscorelib.py
@@ -2,10 +2,17 @@ import os
 import unittest
 
 import boto3
+import hydra
 from moto import mock_s3
 from omegaconf import OmegaConf
 
 from examples.objects_core_lib.core_lib.objects_core_lib import ObjectsCoreLib
+
+# config_file = 'config.yaml'
+# hydra.initialize(config_path='../examples/objects_core_lib/config')
+# config = hydra.compose(config_file)
+#
+# objects_core_lib = ObjectsCoreLib(config.core_lib)
 
 
 class ObjectsCoreLibTesting(ObjectsCoreLib):
@@ -17,10 +24,18 @@ class ObjectsCoreLibTesting(ObjectsCoreLib):
 
 class TestExamples(unittest.TestCase):
     @mock_s3
-    def test(self):
+    def test_object_core_lib(self):
+        upload_file_path = os.path.join(os.path.dirname(__file__), 'test_data\koala.jpeg')
+        download_file_path = os.path.join(os.path.dirname(__file__), 'test_data\koalaAWS.jpeg')
+
         objects_core_lib = ObjectsCoreLibTesting()
+
         conn = boto3.resource('s3', region_name='us-east-1')
         conn.create_bucket(Bucket='mybucket')
-        objects_core_lib.object.set_object('mybucket', os.path.join(os.path.dirname(__file__), 'test_data\koala.jpeg'))
-        obj = objects_core_lib.object.get_object('mybucket', os.path.join(os.path.dirname(__file__), 'test_data\koala.jpeg'))
-        print(obj)
+
+        objects_core_lib.object.set_object('mybucket', upload_file_path, 'koala.jpeg')
+        obj = objects_core_lib.object.get_object('mybucket', download_file_path, 'koala.jpeg')
+
+        self.assertEqual(download_file_path, obj.name)
+        self.assertTrue(os.path.isfile(obj.name))
+
