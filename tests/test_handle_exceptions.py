5b896e9e975b380f6c05c9e44b054a4eecdfebaf
shubham-shay-te <shubham@shay-te.com>
2022-03-03 13:36:04 +0530

    fixed django settings configure error

--- tests/test_handle_exceptions.py
+++ tests/test_handle_exceptions.py
@@ -6,15 +6,14 @@ from django.conf import settings
 
 from core_lib.error_handling.status_code_exception import StatusCodeException
 from core_lib.web_helpers.decorators import handle_exceptions
-from core_lib.web_helpers.request_response_helpers import response_json
 from core_lib.web_helpers.web_helprs_utils import WebHelpersUtils
 
-settings.configure()
-settings.DEFAULT_CHARSET = 'utf-8'
+if not settings.configured:
+    settings.configure()
+    settings.DEFAULT_CHARSET = 'utf-8'
 
 
 class TestHandleExceptions(unittest.TestCase):
-
     def test_raises_exception_django(self):
         web_util = WebHelpersUtils()
         web_util.init(web_util.ServerType.DJANGO)
