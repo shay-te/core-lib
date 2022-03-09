25544a867a408bbff604096c741edb86d9dc27a9
shubham-shay-te <shubham@shay-te.com>
2022-03-09 18:40:56 +0530

    updated generator

--- core_lib_generator/core_lib_config_generator.py
+++ core_lib_generator/core_lib_config_generator.py
@@ -1,5 +1,6 @@
 from omegaconf import OmegaConf
 
+from core_lib.helpers.shell_utils import input_str
 from core_lib_generator.core_lib_template_generator import (
     generate_db_template,
     generate_solr_template,
@@ -10,16 +11,31 @@ from core_lib_generator.core_lib_template_generator import (
     generate_db_config,
 )
 
+print('\nPlease fill out the requested Database information.')
 db = generate_db_template()
+
+print('\nPlease fill out the requested solr information.')
 solr = generate_solr_template()
+
+print('\nPlease fill out the requested Cache information.')
 cache = generate_cache_template()
+
+print('\nPlease fill out the requested information for creating entities in Database.')
 db_entity = generate_db_entity_template()
+
+print('\nPlease fill out the requested information for creating Data Access for entities.')
 data_access = generate_data_access_template(list(db_entity.keys()))
+
+print('\nPlease fill out the requested information for Job.')
 job = generate_job_template()
+
+print('\nPlease fill out the requested information for Database configuration.')
 db_config = generate_db_config()
 
+core_lib_name = input_str('Please enter the name for your Core-lib: ', 'MyCoreLib')
 conf = OmegaConf.create(
     {
+        'core_lib_name': core_lib_name,
         'db': db,
         'solr': solr,
         'cache': cache,
@@ -29,5 +45,5 @@ conf = OmegaConf.create(
     }
 )
 print(OmegaConf.to_yaml(conf))
-with open('core_lib_config.yaml', 'w+') as file:
+with open(f'{core_lib_name}.yaml', 'w+') as file:
     OmegaConf.save(config=conf, f=file.name)
