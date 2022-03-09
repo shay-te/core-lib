24d0bedda96caba9d9dc03004d64280452ab9cb9
shubham-shay-te <shubham@shay-te.com>
2022-03-09 16:44:12 +0530

    added yaml generator

--- core_lib_generator/core_lib_config_generator.py
+++ core_lib_generator/core_lib_config_generator.py
@@ -1,3 +1,5 @@
+from omegaconf import OmegaConf
+
 from core_lib_generator.core_lib_template_generator import (
     generate_db_template,
     generate_solr_template,
@@ -12,6 +14,21 @@ db = generate_db_template()
 solr = generate_solr_template()
 cache = generate_cache_template()
 db_entity = generate_db_entity_template()
-data_access = generate_data_access_template()
+# data_access = generate_data_access_template()
 job = generate_job_template()
-db_config = generate_db_config
+db_config = generate_db_config()
+
+conf = OmegaConf.create(
+    {
+        'db': db,
+        'solr': solr,
+        'cache': cache,
+        'db_entity': db_entity,
+        'job': job,
+        'db_config': db_config,
+    }
+)
+print(OmegaConf.to_yaml(conf))
+file = open('core_lib_config.yaml', 'w+')
+OmegaConf.save(config=conf, f=file.name)
+file.close()
