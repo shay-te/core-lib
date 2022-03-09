24ed47ac5c2cd7a7f1f107a253f7d17c0a9543d3
shubham-shay-te <shubham@shay-te.com>
2022-03-09 18:01:09 +0530

    added support for data access entities

--- core_lib_generator/core_lib_config_generator.py
+++ core_lib_generator/core_lib_config_generator.py
@@ -14,7 +14,7 @@ db = generate_db_template()
 solr = generate_solr_template()
 cache = generate_cache_template()
 db_entity = generate_db_entity_template()
-data_access = generate_data_access_template()
+data_access = generate_data_access_template(list(db_entity.keys()))
 job = generate_job_template()
 db_config = generate_db_config()
 
