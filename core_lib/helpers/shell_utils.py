68a0f0d5579f8bbef928c6e582e47c9c81f04d60
shubham-shay-te <shubham@shay-te.com>
2022-03-14 13:32:09 +0530

    cleaned while loops

--- core_lib/helpers/shell_utils.py
+++ core_lib/helpers/shell_utils.py
@@ -31,44 +31,48 @@ def _check_yes_no(value) -> bool:
 
 def input_yes_no(title: str, default_value: bool = None) -> bool:
     converted_default_value = _convert_default_yes_no(default_value)
-    take_input = True
-    while take_input:
+    result = None
+    while result is None:
         user_input = input(f'{title} (yes/no) {_print_default(converted_default_value)}: ')
         user_input = _get_value(user_input, converted_default_value)
         if user_input in ['yes', 'y', 'no', 'n']:
-            return _check_yes_no(user_input)
+            result = _check_yes_no(user_input)
+    return result
 
 
 def input_str(title: str, default_value: str = None, allow_empty: bool = False) -> str:
-    take_input = True
-    while take_input:
+    result = None
+    while result is None:
         user_input = input(f'{title} {_print_default(default_value)}: ')
         user_input = _get_value(user_input, default_value)
         if allow_empty and str(user_input).strip() == '':
-            return str(user_input).strip()
+            result = str(user_input).strip()
         if str(user_input).strip():
-            return str(user_input).strip().strip('"').strip("'")
+            result = str(user_input).strip().strip('"').strip("'")
+    return result
 
 
 def input_int(title: str, default_value: int = None) -> int:
-    take_input = True
-    while take_input:
+    result = None
+    while result is None:
         user_input = input(f'{title} {_print_default(default_value)}: ')
         user_input = _get_value(user_input, default_value)
         if is_int(user_input):
-            return int(user_input)
+            result = int(user_input)
+    return result
 
 
 def input_bool(title: str, default_value: str = None) -> bool:
-    take_input = True
-    while take_input:
+    result = None
+    while result is None:
         user_input = input(f'{title} {_print_default(default_value)}: ')
         user_input = _get_value(user_input, default_value)
         if user_input.lower() in ['true', 'false', '0', '1']:
             if user_input.lower in ['true', '1']:
-                return True
+                result = True
             else:
-                return False
+                result = False
+    return result
 
 
 def input_enum(enum_class: enum, title: str, default_value: int = None) -> int:
@@ -76,19 +80,21 @@ def input_enum(enum_class: enum, title: str, default_value: int = None) -> int:
     for item in enum_class:
         enum_values.add(item.value)
         print(f'{item.value}-{item.name}')
-    take_input = True
-    while take_input:
+    result = None
+    while result is None:
         user_input = input(f'{title} {_print_default(default_value)}: ')
         user_input = _get_value(user_input, default_value)
         if is_int(user_input) and int(user_input) in enum_values:
-            return int(user_input)
+            result = int(user_input)
+    return result
 
 
 def input_list(list_value: list, title: str, default_value: int = None):
     [print(f'{list_value.index(i) + 1}-{i}') for i in list_value]
-    take_input = True
-    while take_input:
+    result = None
+    while result is None:
         user_input = input(f'{title} {_print_default(default_value)}: ')
         user_input = _get_value(user_input, default_value)
         if is_int(user_input) and int(user_input) <= len(list_value) or int(user_input) > 0:
-            return list_value[int(user_input) - 1]
+            result = list_value[int(user_input) - 1]
+    return result
