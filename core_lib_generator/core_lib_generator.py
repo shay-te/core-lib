0b0bfe9004204ae9cf76374ccc3a7b57ea882eb8
shubham-shay-te <shubham@shay-te.com>
2022-03-14 14:08:43 +0530

    creating structure from yaml

--- core_lib_generator/core_lib_generator.py
+++ core_lib_generator/core_lib_generator.py
@@ -31,6 +31,7 @@ if not os.path.isdir(f'{camel_to_snake(core_lib_name)}/{camel_to_snake(core_lib_
     for name in db_entities_list
 ]
 for name in data_access_list:
+    print('is_crud' in core_lib_data_access[name])
     if 'is_crud_soft_delete_token' in core_lib_data_access[name]:
         shutil.copy(
             f'{camel_to_snake(core_lib_name)}/{camel_to_snake(core_lib_name)}/data_layers/data_access/template_crud_soft_delete_token_data_access.py',
---
+++ core_lib_generator/my_core_lib/my_core_lib/data_layers/data_access/template_crud_soft_delete_data_access.py
@@ -0,0 +1,9 @@
+from core_lib.data_layers.data.handler.sql_alchemy_data_handler_registry import SqlAlchemyDataHandlerRegistry
+from core_lib.data_layers.data_access.db.crud.crud import CRUD
+from core_lib.data_layers.data_access.db.crud.crud_soft_data_access import CRUDSoftDeleteDataAccess
+from core_lib.rule_validator.rule_validator import RuleValidator
+
+
+class TemplateCRUDSoftDeleteDataAccess(CRUDSoftDeleteDataAccess):
+    def __init__(self, db_entity, db: SqlAlchemyDataHandlerRegistry, rule_validator: RuleValidator):
+        CRUD.__init__(self, db_entity, db, rule_validator)
