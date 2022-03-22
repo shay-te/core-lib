599b7f3022c36d3a1f6037a94fac61fd98e6bc35
shubham-shay-te <shubham@shay-te.com>
2022-03-22 18:03:33 +0530

    generated files cleaning

--- core_lib_generator/facebook_core_lib/core_lib/jobs/my_job.py
+++
@@ -1,9 +0,0 @@
-from core_lib.jobs.job import Job
-
-
-class UpdateSeller(Job):
-    def initialized(self, data_handler):
-        pass
-
-    def run(self):
-        pass
--- core_lib_generator/facebook_core_lib/hydra_plugins/facebook_core_lib.py
+++
@@ -1,8 +0,0 @@
-from hydra.plugins.search_path_plugin import SearchPathPlugin
-from hydra.core.config_search_path import ConfigSearchPath
-
-
-class FacebookCoreLibSearchPathPlugin(SearchPathPlugin):
-    def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
-        assert isinstance(search_path, ConfigSearchPath)
-        search_path.append("facebook_core_lib", "pkg://facebook_core_lib.core_lib.config")
