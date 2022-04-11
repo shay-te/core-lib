c6fd249f357a922e571446745975fb069afdb24a
shubham-shay-te <shubham@shay-te.com>
2022-04-11 11:12:38 +0530

    soft delete token  default value = 0 for indexing

--- core_lib/data_layers/data/db/sqlalchemy/mixins/soft_delete_token_mixin.py
+++ core_lib/data_layers/data/db/sqlalchemy/mixins/soft_delete_token_mixin.py
@@ -2,5 +2,5 @@ from sqlalchemy import Column, Integer
 
 
 class SoftDeleteTokenMixin(object):
-    deleted_at_token = Column(Integer, default=None)
+    deleted_at_token = Column(Integer, default=0)
     deleted_at_token._creation_order = 9998
