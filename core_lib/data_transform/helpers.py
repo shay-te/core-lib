c9839ad76fc6feae49f1d470c87b7a1df0faa87a
shubham-shay-te <shubham@shay-te.com>
2022-05-02 11:49:13 +0530

    in progress: working with all different keys

--- core_lib/data_transform/helpers.py
+++ core_lib/data_transform/helpers.py
@@ -1,24 +1,40 @@
 def get_dict_attr(obj: dict, path: str):
     path_list = path.split('.')
-    obj_ptr = obj
+    obj_temp = obj
     try:
         for elem in path_list:
-            obj_ptr = obj_ptr[elem]
-        print(obj_ptr)
+            obj_temp = obj_temp[elem]
+        print(obj_temp)
     except KeyError:
         print('Not found')
 
 
 def set_dict_attr(obj: dict, path: str, value):
     path_list = path.split('.')
-    obj_ptr = obj
-    for elem in path_list:
-        if elem == path_list[-1]:
-            print(elem)
-            obj_ptr[elem] = value
-    print(obj_ptr)
+    path_len = len(path_list)
+    updated_obj = obj
+    if path_len == 1:
+        updated_obj[path_list[0]] = value
+    elif path_len == 2:
+        updated_obj = _update(obj, path_list, value)
+    else:
+        updated_obj = _update(obj, path_list, value)
+        for index in range(1, path_len):
+            updated_path_list = path_list[:-index]
+            if len(updated_path_list) >= 2:
+                updated_obj = _update(obj, updated_path_list, updated_obj)
+    print(updated_obj)
+
+
+def _update(obj: dict, path: list, new_value) -> dict:
+    for key, value in obj.items():
+        if isinstance(value, dict):
+            if key == path[-2]:
+                value.setdefault(path[-1], new_value)
+                return {key: value}
+            return _update(value, path, new_value)
 
 
 if __name__ == '__main__':
-    set_dict_attr({'person': {'name': 'aaad', 'age': 131, 'object': {'person': {'name': 'aaad', 'age': 131}}}},
-                  'person.object.salary', 20000)
+    print(set_dict_attr({'person': {'name': 'aaad', 'age': 131, 'object': {'person2': {'name': 'name', 'age': 478}}}},
+                  'person.object.person2.name', 'adas'))
