ca6c290d81dfdb81dcdf705504592fe74f01e783
shubham-shay-te <shubham@shay-te.com>
2022-07-01 16:09:56 +0530

    formatting

--- website/src/fieldsGenerator/exportYamlFields.js
+++ website/src/fieldsGenerator/exportYamlFields.js
@@ -5,8 +5,8 @@ export const exportYamlFields = () => {
     fields.push(
         {
             title: 'Export YAML file',
-            subtitle: 'Will generate and download a new YAML file with the configured Core-Lib generator config.' +
-                      'This YAML contains the data that will help the generator generate a new Core-Lib for you' +
+            subtitle: 'Will generate and download a new YAML file with the configured Core-Lib generator config. ' +
+                      'This YAML contains the data that will help the generator generate a new Core-Lib for you. ' +
                       'You can either select the Download Zip option and download the zip file of your Core-Lib right away or '+
                       'you can use the command `core_lib -g YourCoreLib.yaml` to generate a new Core-Lib from the command line.',
             type: 'title_subtitle',
