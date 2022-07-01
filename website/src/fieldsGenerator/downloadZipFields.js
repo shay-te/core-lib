033493cde22c7a0f3d14409923c06bc687e8d960
shubham-shay-te <shubham@shay-te.com>
2022-07-01 16:07:03 +0530

    add instructions

--- website/src/fieldsGenerator/downloadZipFields.js
+++ website/src/fieldsGenerator/downloadZipFields.js
@@ -6,7 +6,8 @@ export const downloadZipFields = () => {
     fields.push(
         {
             title: 'Download Core-Lib zip file',
-            subtitle: 'Will generate and download a new Core-Lib zip file as configured in the generator.',
+            subtitle: 'Will generate and download a new Core-Lib zip file as configured in the generator. ' +
+                      'After you have the zip file with you, you can then place the Core-Lib inside your project and start using it without any hassle.',
             type: 'title_subtitle',
             // validatorCallback: validateFunc,
         },
