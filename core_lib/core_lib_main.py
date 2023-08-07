15b09e094c6d2a115a39b1cf227054c980f3cbfa
shay.te@gmail.com <shay.te@gmail.com>
2023-08-07 12:00:37 +0300

    fix command line tool

--- core_lib/core_lib_main.py
+++ core_lib/core_lib_main.py
@@ -1,68 +1,32 @@
 #!/usr/bin/env python
-import argparse
 import logging
 import os
-from typing import Callable
-
-import hydra
-from omegaconf import DictConfig
-
 from core_lib.alembic.alembic import Alembic
-from hydra import compose, initialize_config_dir
+from hydra import compose, initialize
+from core_lib_template.core_lib_generate import CoreLibGenerate
+import click
 
 from core_lib.helpers.validation import is_int
-from core_lib_generator.core_lib_generator_from_yaml import CoreLibGenerator
-from core_lib_generator.core_lib_config_generate_yaml import get_data_from_user
-
-logger = logging.getLogger(__name__)
-
-hydra.core.global_hydra.GlobalHydra.instance().clear()
 
 
-def list_to_string(lst: list) -> str:
-    return ' '.join(lst)
+def list_to_string(lst: list):
+    name = ''
+    for n in lst:
+        name = name + n + ' '
+    return name
 
 
-def on_create():
-    get_data_from_user()
+def on_create(value):
+    CoreLibGenerate().new(list_to_string(value))
 
 
 def on_generate(value):
-    initialize_config_dir(config_dir=os.getcwd())
-    CoreLibGenerator(compose(list_to_string(value))).run_all()
-    print('Core-Lib Generated!')
+    CoreLibGenerate().generate(list_to_string(value))
 
 
-def on_revision(value):
-    config = load_config()
-    alembic = Alembic(os.path.join(os.getcwd(), config.core_lib_module), config)
-    logging.getLogger('alembic').setLevel(logging.INFO)
 
-    command = value.pop(0)
-    logger.info(f'revision to `{command}`')
-
-    if command == 'head':
-        alembic.upgrade('head')
-    elif command == 'base':
-        alembic.downgrade('base')
-    elif command == 'list':
-        alembic.history()
-    elif is_int(command):
-        number = int(command)
-        if number >= 0:
-            alembic.upgrade(f'+{number}')
-        else:
-            alembic.downgrade(f'{number}')
-    elif command == 'new':
-        name = list_to_string(value)
-        logger.info('creating new migrating named: `{}`'.format(name))
-        alembic.create_migration(name)
-    else:
-        logger.error(f'unknown command `{command}`')
-
-
-def get_rev_options() -> list:
-    choices = ['head', 'base']
+def get_rev_options():
+    choices = ['head', 'base', 'new']
     for i in range(-10, 11):
         if i != 0:
             choices.append(str(i))
@@ -71,53 +35,73 @@ def get_rev_options() -> list:
     return choices
 
 
-def load_config() -> DictConfig:
-    initialize_config_dir(config_dir=os.getcwd())
+def load_config():
+    path_cwd = os.getcwd()
+    path_folder = os.path.dirname(os.path.abspath(__file__))
+    path_rel = os.path.relpath(path_cwd, path_folder)
+    # initialize(config_path=os.path.join(path_rel))
+    initialize(config_path=path_rel)
     return compose('core_lib_config.yaml')
 
+#
+# def main():
+#     parser = argparse.ArgumentParser(description="Core-Lib")
+#     g = parser.add_mutually_exclusive_group()
+#     g.add_argument('-c', '--create', nargs=1, help='Create new core-lib')
+#     g.add_argument('-g', '--generate', nargs=1, help='Generate core-lib classes')
+#     g.add_argument('-r', '--revision', nargs=1, choices=get_rev_options(), help=f'Database migration.')
+#     args = parser.parse_args()
+#     if args.create:
+#         on_create(args.create)
+#     elif args.generate:
+#         on_generate(args.generate)
+#     elif args.revision:
+#         on_revision(args.revision)
+#     else:
+#         print(parser.print_help())
 
+
+@click.group()
 def main():
-    logging.basicConfig(level=logging.INFO)
-    parser = argparse.ArgumentParser(description="Core-Lib")
-    g = parser.add_mutually_exclusive_group()
-    g.add_argument('-g', '--generate', nargs=1, help='Generate Core-Lib classes from YAML file')
-    subparsers = parser.add_subparsers(dest='command')
-    rev = subparsers.add_parser('rev', help='migration revision')
-    rev.add_argument(
-        '-m',
-        '--migrate',
-        help='Existing migration select from the list',
-        choices=get_rev_options(),
-        nargs=1,
-    )
-    rev.add_argument(
-        '-n',
-        '--new',
-        help='New migration name to be created',
-        nargs=1,
-    )
-    rev.add_argument(
-        '-l',
-        '--list',
-        help='List of revisions',
-        nargs='?',
-        const='list'
-    )
-    args = parser.parse_args()
-    if args.command == 'rev':
-        if args.migrate:
-            on_revision(args.migrate)
-        elif args.new:
-            on_revision(['new', args.new[0]])
+    pass
+
+@click.command()
+def create():
+    click.echo('Initialized the database')
+
+@click.command()
+def generate():
+    click.echo('Dropped the database')
+
+@click.command()
+@click.option('--rev', help=' '.join(get_rev_options()))
+@click.option('--name', help='name of new revision')
+def migrate(rev, name):
+    config = load_config()
+    alembic = Alembic(os.path.join(os.getcwd(), config.core_lib_module), config)
+    logging.getLogger('alembic').setLevel(logging.INFO)
+
+    if rev == 'head':
+        click.echo(f'revision to `{rev}`')
+        alembic.upgrade('head')
+    elif rev == 'base':
+        click.echo(f'revision to `{rev}`')
+        alembic.downgrade('base')
+    elif is_int(rev):
+        click.echo(f'revision to `{rev}`')
+        number = int(rev)
+        if number >= 0:
+            alembic.upgrade('+{}'.format(number))
         else:
-            on_revision(['list'])
-    elif args.generate:
-        on_generate(args.generate)
-    elif args.revision:
-        on_revision(args.revision)
-    else:
-        print(parser.print_help())
+            alembic.downgrade(str(number))
+    elif rev == 'new':
+        if name:
+            click.echo(f'new revision named `{name}`')
+            click.echo(f'\n\n\n------------{os.environ["POSTGRES_USER"]}')
+            alembic.create_migration(name)
+        else:
+            click.echo(f'--name parameter is mandatory when creating a new revision')
 
-
-if __name__ == '__main__':
-    main()
+main.add_command(create)
+main.add_command(generate)
+main.add_command(migrate)
