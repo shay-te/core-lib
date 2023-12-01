1557ab9e9f5a00d9be02962e61ef509402d3abed
“AnshPaktolus” <ansh@shay-te.com>
2023-12-01 17:36:54 +0530

    Added tests for duplicate error decorator

--- tests/test_error_handling.py
+++ tests/test_error_handling.py
@@ -1,25 +1,39 @@
 import unittest
 
+from core_lib.error_handling.duplicate_error_decorator import DuplicateErrorHandler
 from core_lib.error_handling.not_found_decorator import NotFoundErrorHandler
 from core_lib.error_handling.status_code_assert import StatusCodeAssert
 from core_lib.error_handling.status_code_exception import StatusCodeException
+from sqlalchemy import exc
 
 str_value = 'str_value'
 tuple_value = ("fruit", "apple")
 
 
 class TestErrorHandling(unittest.TestCase):
-    def test_error_handler_string(self):
+    def test_not_found_error_handler_string(self):
         with self.assertRaises(StatusCodeException):
-            self.get_nothing()
+            self.get_nothing_with_not_found_error_handler()
 
-        self.assertEqual(self.get_string(), str_value)
+        self.assertEqual(self.get_string_with_not_found_error_handler(), str_value)
 
-    def test_error_handler_tuple(self):
+    def test_not_found_error_handler_tuple(self):
         with self.assertRaises(StatusCodeException):
-            self.get_nothing()
+            self.get_nothing_with_not_found_error_handler()
 
-        self.assertTupleEqual(self.get_tuple(), tuple_value)
+        self.assertTupleEqual(self.get_tuple_with_not_found_error_handler(), tuple_value)
+
+    def test_duplicate_error_handler_string(self):
+        with self.assertRaises(StatusCodeException):
+            self.get_integrity_error_with_duplicate_error_handler()
+
+        self.assertEqual(self.get_string_with_duplicate_error_handler(), str_value)
+
+    def test_duplicate_error_handler_tuple(self):
+        with self.assertRaises(StatusCodeException):
+            self.get_integrity_error_with_duplicate_error_handler()
+
+        self.assertTupleEqual(self.get_tuple_with_duplicate_error_handler(), tuple_value)
 
     def test_status_code_assert(self):
         with self.assertRaises(StatusCodeException):
@@ -27,13 +41,28 @@ class TestErrorHandling(unittest.TestCase):
                 assert True is False
 
     @NotFoundErrorHandler()
-    def get_string(self):
+    def get_string_with_not_found_error_handler(self):
         return str_value
 
     @NotFoundErrorHandler()
-    def get_tuple(self):
+    def get_tuple_with_not_found_error_handler(self):
         return tuple_value
 
     @NotFoundErrorHandler()
-    def get_nothing(self):
+    def get_nothing_with_not_found_error_handler(self):
+        # Method is empty to check that it raises not found error
         pass
+
+    @DuplicateErrorHandler()
+    def get_integrity_error_with_duplicate_error_handler(self):
+        raise exc.IntegrityError("Simulated IntegrityError", {}, None)
+
+    @DuplicateErrorHandler()
+    def get_string_with_duplicate_error_handler(self):
+        return str_value
+
+    @DuplicateErrorHandler()
+    def get_tuple_with_duplicate_error_handler(self):
+        return tuple_value
+
+
