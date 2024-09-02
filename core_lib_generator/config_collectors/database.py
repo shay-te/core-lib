cc3037d368ce82b77f4cd3efa1a4592cc9b1d165
Oren <xxorenxx@gmail.com>
2024-09-02 09:26:08 +0300

    every option gets sql alchemy

--- core_lib_generator/config_collectors/database.py
+++ core_lib_generator/config_collectors/database.py
@@ -86,9 +86,7 @@ def _generate_db_config(
 ) -> dict:
     config = _build_url(db_type, db_name, db_username, db_password, db_port, db_host)
     if db_type:
-        db_type: str = 'core_lib.connection'
-        if migrate:
-            db_type += '.sql_alchemy_connection_registry.SqlAlchemyConnectionRegistry'
+        db_type: str = 'core_lib.connection.sql_alchemy_connection_registry.SqlAlchemyConnectionRegistry'
     return {
         'env': config['env'],
         'connection': {
