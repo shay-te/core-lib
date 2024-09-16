a54873d57933d95598b669a952864e800db24d30
Oren <xxorenxx@gmail.com>
2024-09-16 13:25:30 +0300

    pull requests revets

--- core_lib_generator/file_generators/test_util_generator.py
+++ core_lib_generator/file_generators/test_util_generator.py
@@ -7,11 +7,10 @@ class UtilTestGenerateTemplate(TemplateGenerator):
     def generate(self, template_content: str, yaml_data: dict, core_lib_name: str, file_name: str) -> str:
         camel_case_name: str = snake_to_camel(core_lib_name)
 
-        import_instance_string: str = f'from {core_lib_name}_instance import {camel_case_name}'
-        template_content = template_content.replace('# template core_lib_instance', import_instance_string)
-
-        core_lib_import: str = f'from {os.path.basename(os.getcwd())}.{core_lib_name}.{core_lib_name}.{core_lib_name} import {camel_case_name}'
+        core_lib_import: str = f'from {core_lib_name}.{core_lib_name} import {camel_case_name}'
         template_content = template_content.replace('# template_core_lib_import', core_lib_import)
+
+        template_content = template_content.replace('TemplateCoreLibClass', camel_case_name)
 
         template_class: str = f'{camel_case_name}Instance'
         template_content = template_content.replace('TemplateInstance', template_class)
