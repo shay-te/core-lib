f136b4dd684bf456498512c88d87979c4f00f030
Oren <xxorenxx@gmail.com>
2024-09-18 14:37:32 +0300

    made server type var (not a magic number) enum

--- core_lib_generator/core_lib_config_generate_yaml.py
+++ core_lib_generator/core_lib_config_generate_yaml.py
@@ -1,6 +1,6 @@
 from omegaconf import OmegaConf
-
-from core_lib.helpers.shell_utils import input_str, input_yes_no, input_int
+from enum import Enum
+from core_lib.helpers.shell_utils import input_str, input_yes_no, input_enum
 from core_lib.helpers.string import any_to_pascal
 from core_lib_generator.config_collectors.cache import generate_cache_template
 from core_lib_generator.config_collectors.data_access import generate_data_access_template
@@ -21,6 +21,12 @@ def _get_env_variables(data):
         return env_variables
 
 
+class ServerType(Enum):
+    DJANGO = 1
+    FLASK = 2
+    NOSERVER = 3
+
+
 config = {}
 config.setdefault('data', [])
 config.setdefault('cache', [])
@@ -32,7 +38,7 @@ data_layers = {}
 data_layers.setdefault('data', [])
 data_layers.setdefault('data_access', [])
 data_layers.setdefault('service', [])
-server_type = 3
+server_type: int
 
 
 def _get_data_layers_config():
@@ -106,13 +112,13 @@ def create_yaml_file(core_lib_name: str):
 
 
 def ask_server_type() -> int:
-    return input_int('\nWhat kind of server type would you like?\n'
-                     '1-Django\n'
-                     '2-Flask\n'
-                     '3-None\n',
-                     3,
-                     True
-                     )
+    return input_enum(ServerType,
+                      '\nWhat kind of server type would you like?\n'
+                      '1-Django\n'
+                      '2-Flask\n'
+                      '3-None\n',
+                      default_value=ServerType.NOSERVER.value,
+                      )
 
 
 def generate_core_lib_yaml():
@@ -128,7 +134,8 @@ def generate_core_lib_yaml():
     if want_job:
         _get_jobs_config(core_lib_name)
 
-    server_type: int = ask_server_type()
+    global server_type
+    server_type = ask_server_type()
 
     want_setup = input_yes_no('\nDo you want to add setup.py?', True)
     if want_setup:
