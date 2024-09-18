099371d4bcc0e04da3c9291885c0d2e96723e98f
Oren <xxorenxx@gmail.com>
2024-09-18 10:04:41 +0300

    added server type to core lib instance added a test to the generator from yaml

--- core_lib_generator/file_generators/template_core_lib_instance_generator.py
+++ core_lib_generator/file_generators/template_core_lib_instance_generator.py
@@ -7,6 +7,14 @@ class CoreLibInstanceGenerate(TemplateGenerator):
     def generate(self, template_content: str, yaml_data: dict, core_lib_name: str, file_name: str) -> str:
         camel_case_class: str = snake_to_camel(core_lib_name)
 
+        if yaml_data.get('server_type'):
+            if yaml_data['server_type'] == 3:
+                template_content = template_content.replace('# web_helper_template', '')
+            elif yaml_data['server_type'] == 2:
+                template_content = template_content.replace('# web_helper_template', 'WebHelpersUtils.init(WebHelpersUtils.ServerType.FLASK)')
+            elif yaml_data['server_type'] == 1:
+                template_content = template_content.replace('# web_helper_template', 'WebHelpersUtils.init(WebHelpersUtils.ServerType.DJANGO)')
+
         core_lib_import = f'from {os.path.basename(os.getcwd())}.{core_lib_name}.{core_lib_name}.{core_lib_name} import {camel_case_class}'
         template_content = template_content.replace('# template_core_lib_import', core_lib_import)
 
